bc492d55f501bf51f71c8f0348f4731c400edbb4
DAZAI <kandarp.mishra77@gmail.com>
2025-09-09 19:48:30 +0530

    learn the latch basic SR (NOR ) , the nand latch is simple just opp. invalid shifts to the 0 , 0 condition instead  of the 1 ,1 .

--- basics/unit-2/latches.v
+++ basics/unit-2/latches.v
@@ -1,5 +1,29 @@
-module latches (
-
+module sr_latch (
+    input wire S,     // Set input
+    input wire R,     // Reset input
+    output reg Q,     // Main output
+    output reg Qbar   // Complement output
 );
-    
+
+always @(*) begin
+    if (S && R) begin
+        // Invalid condition
+        Q     = 1'bx;   // Unknown
+        Qbar  = 1'bx;
+    end 
+    else if (S) begin
+        Q     = 1'b1;   // Set
+        Qbar  = 1'b0;
+    end 
+    else if (R) begin
+        Q     = 1'b0;   // Reset
+        Qbar  = 1'b1;
+    end 
+    // Hold state
+    else begin
+        Q     = Q;      // keep previous input whatever it was be it reset = 0 or set = 1 : )
+        Qbar  = Qbar;
+    end
+end
+
 endmodule
